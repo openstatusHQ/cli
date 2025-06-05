69b03dc4e5d9ab8946bd4157825733ff5e34fda3
Thibault Le Ouay <thibaultleouay@gmail.com>
2025-06-05 21:32:02 +0200

    ⬆️ Version 0.0.4

--- cmd/openstatus/main.go
+++ cmd/openstatus/main.go
@@ -16,7 +16,7 @@ func main() {
 		Name:    "openstatus",
 		Usage:   "This is OpenStatus Command Line Interface",
 		Description: "OpenStatus is a command line interface for managing your monitors and triggering your synthetics tests. \n\nPlease report any issues at https://github.com/openstatusHQ/cli/issues/new",
-		Version: "v0.0.3",
+		Version: "v0.0.4",
 		Commands: []*cli.Command{
 			monitors.MonitorsCmd(),
 			run.RunCmd(),

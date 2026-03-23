22785b43b4020dc0493a519ba6bca6ec9317d742
Thibault Le Ouay <thibaultleouay@gmail.Com>
2026-03-23 17:28:09 +0100

    version dump (#22)

--- internal/cmd/app_test.go
+++ internal/cmd/app_test.go
@@ -20,8 +20,8 @@ func Test_NewApp(t *testing.T) {
 			t.Errorf("Expected app name 'openstatus', got %s", app.Name)
 		}
 
-		if app.Version != "v1.0.1" {
-			t.Errorf("Expected version 'v1.0.1', got %s", app.Version)
+		if app.Version != "v1.0.2" {
+			t.Errorf("Expected version 'v1.0.2', got %s", app.Version)
 		}
 
 		if !app.Suggest {
